5d90db7e7b773344ab32c80bbde42f229e12174d
gabutox <gabizonala@gmail.com>
2026-06-17 09:33:39 -0300

    Agrega clase main y configura punto de entrada

---
+++ proyecto_lenguaje_ii/src/proyecto_lenguaje_ii/Proyecto_lenguaje_ii.java
@@ -0,0 +1,8 @@
+package proyecto_lenguaje_ii;
+
+public class Proyecto_lenguaje_ii {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new w_login().setVisible(true));
+    }
+}
